1d0dde2cc35bdcb1b7d62ed16cc538bb27e17ad1
Chance Vodnoy <esgameco@gmail.com>
2022-06-06 19:26:26 -0700

    Updated coc install

--- bin/installs/coc.sh
+++ bin/installs/coc.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-vim +'silent :CocInstall coc-clangd' +qall
-vim +'silent :CocCommand clangd.install' +qall
-vim +'silent :CocInstall coc-sh' +qall
-vim +'silent :CocInstall coc-cmake' +qall
-vim +'silent :CocInstall coc-python' +qall
-vim +'silent :CocInstall coc-java' +qall
-vim +'silent :CocInstall coc-git' +qall
+vim +'CocInstall coc-clangd' +'CocInstall coc-sh' +'CocInstall coc-cmake' +'CocInstall coc-python' +'CocInstall coc-java' +'CocInstall coc-git'
+vim main.c +"CocCommand clangd.install"
